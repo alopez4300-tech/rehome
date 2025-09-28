3da5b0361b732ed1329712a125633b2320499daa
alopez4300-tech <alopez4300@gmail.com>
2025-09-28 03:37:23 +0000

    Add Dashboard and AdminPanelProvider classes; update CORS and Sanctum configurations; enhance web routes with API info

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -16,8 +16,9 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        '%s%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,',
+        'shiny-broccoli-pj5j5pw67xj7c7jj-3000.app.github.dev,',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
